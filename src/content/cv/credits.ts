5ab0f4c242b0613df7f1ed4f6ad31fe8d9009aeb
Nik Chanda <nikchanda@gmail.com>
2023-10-17 12:19:18 -0500

    updated header and credits

---
+++ src/content/cv/credits.ts
@@ -0,0 +1,26 @@
+import brandAstro from '~/assets/images/brand-astro.svg';
+import brandSvelte from '~/assets/images/brand-svelte.png';
+import brandPreact from '~/assets/images/brand-preact.ico';
+import brandDaisyUI from '~/assets/images/brand-daisyui.svg';
+import brandOnWidget from '~/assets/images/brand-onwidget.ico';
+import brandPocketbase from '~/assets/images/brand-pocketbase.ico';
+import brandGithub from '~/assets/images/brand-github.png';
+import brandFlaticon from '~/assets/images/brand-flaticon.ico';
+import brandVercel from '~/assets/images/brand-vercel.ico';
+
+export const credits = [
+  { role: 'Scalable Web Framework', label: 'Astro', href: 'https://astro.build', image: brandAstro },
+  { role: 'Component Framework', label: 'Svelte', href: 'https://svelte.dev/', image: brandSvelte },
+  { role: 'Digital Transformation', label: 'Preact', href: 'https://preactjs.com/', image: brandPreact },
+  { role: 'CSS Framework', label: 'DaisyUI on TailwindCSS', href: 'https://daisyui.com/', image: brandDaisyUI },
+  { role: 'Reference Theme', label: 'Astrowind by onWidget', href: 'https://onwidget.com/', image: brandOnWidget },
+  { role: 'Database / User Auth', label: 'Pocketbase', href: 'https://pocketbase.io/', image: brandPocketbase },
+  { role: 'Version Control w/ CI/CD', label: 'Github', href: 'https://www.github.com', image: brandGithub },
+  { role: 'Frontend Cloud w/ CI/CD', label: 'Vercel', href: 'https://www.vercel.com', image: brandVercel },
+  {
+    role: 'Shortcut Icon',
+    label: 'Abdul-Aziz @ Flaticon',
+    href: 'https://www.flaticon.com/free-icons/add-user',
+    image: brandFlaticon,
+  },
+];
---
+++ src/content/cv/testimonials.ts
@@ -0,0 +1,22 @@
+export const testimonials = [
+  {
+    testimonial: `Nik is an expert in what he does. He was always very meticulously prepared, poised and professional. There were times requiring of him to train me to use the new system from the simplest steps, he was consistently responsive and patient with me and all my questions. It was a pleasure working with Nik.`,
+    name: 'Jenny Au',
+    job: 'Manager, User Experience Engineering',
+    href: 'https://www.linkedin.com/in/jennyau/',
+    image: {
+      src: 'https://media.licdn.com/dms/image/C5603AQGOu9xnwYsynw/profile-displayphoto-shrink_100_100/0/1592100862565?e=1703116800&v=beta&t=MDw8bpgVOcVZ9ZAAWd5Fl_Idclqf4GkBNM0ijBVFMfM',
+      alt: 'Jenny Au Image',
+    },
+  },
+  {
+    testimonial: `Nik always provides great service, creative solutions, and a 'get-er-done' attitude. He has gone above and beyond the call on consistently to get the job done right. There are few people that would trust with my client list, and know that they will be dealt with respectfully and professionally. Nik tops that list.`,
+    name: 'Steve von Ehrenkrook',
+    job: 'President & Founder, CJS Associates',
+    href: 'https://www.linkedin.com/in/cjsassociates/',
+    image: {
+      src: 'https://media.licdn.com/dms/image/C5603AQFh4r70Mf5-YQ/profile-displayphoto-shrink_100_100/0/1524774577686?e=1703116800&v=beta&t=ac5SVK5A7UhzopA-ZQkftUzV63qZy2gU0EGCtO_828M',
+      alt: 'Steve von Ehrenkrook Image',
+    },
+  },
+];
